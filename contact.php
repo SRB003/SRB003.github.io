1b83189c2a47abcb56aa1c599fc9e95c3c754779
Sahil Ramesh Bhuvad <103353851+SRB003@users.noreply.github.com>
2023-10-03 17:52:27 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,41 +1,34 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'sahilbhuvad01926@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+    
+    $to = "sahilbhuvad01926@gmail.com"; 
+    $subject = "New Contact Form Submission";
+    $messageBody = "Name: $name\n";
+    $messageBody .= "Email: $email\n";
+    $messageBody .= "Subject: $sub\n";
+    $messageBody .= "Message:\n$message\n";
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+    
+     if (mail($to, $subject, $messageBody)) {
+        
+        echo '<script>';
+        echo 'alert("Thank you! Your submission has been received!");';
+        echo 'window.history.back();';
+        echo '</script>';
+    } else {
+       
+        echo '<script>';
+        echo 'alert("Oops! Something went wrong while submitting the form.");';
+        echo 'window.history.back();';
+        echo '</script>';
+    }
+}
 ?>
